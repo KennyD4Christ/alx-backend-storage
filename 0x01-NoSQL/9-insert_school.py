64dd1320b9156026a546a1668d416da37a7a18d8
KennyD4Christ <kennyd4christ@gmail.com>
2024-08-13 09:06:50 +0100

    9-insert_school.py

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -1,29 +1,23 @@
 #!/usr/bin/env python3
-""" 9-insert_school """
 
+"""
+Module for Listing all documents in a collection
+"""
+
+from typing import List, Dict
 from pymongo.collection import Collection
 
 
-def insert_school(mongo_collection: Collection, **kwargs) -> str:
-    """Insert a new document into a collection.
+def list_all(mongo_collection: Collection) -> List[Dict]:
+    """ List all documents in a collection.
 
     Args:
         mongo_collection (Collection): The pymongo collection object.
-        **kwargs: Arbitrary keyword arguments representing the document fields.
 
     Returns:
-        str: The _id of the new document.
+        List[Dict]: A list of all documents in the collection.
     """
-    result = mongo_collection.insert_one(kwargs)
-    return str(result.inserted_id)
+    return list(mongo_collection.find())
 
 
 if __name__ == "__main__":
-    # Example code for testing purposes
-    from pymongo import MongoClient
-    client = MongoClient('mongodb://127.0.0.1:27017')
-    school_collection = client.my_db.school
-
-    new_school_id = insert_school(school_collection, name="UCSF",
-                                  address="505 Parnassus Ave")
-    print("New school created: {}".format(new_school_id))
