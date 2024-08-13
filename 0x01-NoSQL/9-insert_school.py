b3f8ffa39bcfcb03d631aab4450b334f19684f9a
KennyD4Christ <kennyd4christ@gmail.com>
2024-08-13 09:01:57 +0100

    9-insert_school.py

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
 """ 9-insert_school """
 
-from pymongo import MongoClient
 from pymongo.collection import Collection
 
 
 def insert_school(mongo_collection: Collection, **kwargs) -> str:
-    """ Insert a new document into a collection.
+    """Insert a new document into a collection.
 
     Args:
         mongo_collection (Collection): The pymongo collection object.
@@ -21,9 +20,10 @@ def insert_school(mongo_collection: Collection, **kwargs) -> str:
 
 if __name__ == "__main__":
     # Example code for testing purposes
+    from pymongo import MongoClient
     client = MongoClient('mongodb://127.0.0.1:27017')
     school_collection = client.my_db.school
 
-    new_school_id = insert_school(school_collection, name="New School",
-                                  location="123 Main St", founded=2024)
-    print(f"Inserted school with _id: {new_school_id}")
+    new_school_id = insert_school(school_collection, name="UCSF",
+                                  address="505 Parnassus Ave")
+    print("New school created: {}".format(new_school_id))
