d03deb2ad84b6f1901d40c5ede326d60ee2bf9d6
KennyD4Christ <kennyd4christ@gmail.com>
2024-08-13 09:14:37 +0100

    8-all.py

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -3,6 +3,7 @@
 Module to list all documents in a MongoDB collection.
 """
 
+
 def list_all(mongo_collection):
     """
     Lists all documents in the specified MongoDB collection.
@@ -15,12 +16,3 @@ def list_all(mongo_collection):
     """
     documents = list(mongo_collection.find())
     return documents if documents else []
-
-if __name__ == "__main__":
-    # Example code for testing purposes
-    from pymongo import MongoClient
-    client = MongoClient('mongodb://127.0.0.1:27017')
-    school_collection = client.my_db.school
-    schools = list_all(school_collection)
-    for school in schools:
-        print("[{}] {}".format(school.get('_id'), school.get('name')))
